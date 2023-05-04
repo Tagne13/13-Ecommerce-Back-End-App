82d18a3a1550f8417cd812ee908055e4a18f42ce
El Jefe <Tagne13@gmail.com>
2023-05-03 21:30:47 -0400

    edited/deleted comments in route files

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
       include: {
@@ -21,7 +20,6 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findOne({
       where: {
